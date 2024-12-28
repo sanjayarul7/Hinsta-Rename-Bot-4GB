d17b0c1a49a3ed0851f2659f5efa076fc5fb42b8
sanjayarul7 <Sanjayvillian1@gmail.com>
2024-12-28 22:05:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ import os
 
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+API_ID = int(os.environ.get("API_ID", "22928570"))
+API_HASH = os.environ.get("API_HASH", "60bb37bddecb48c27c3e86906a077603")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7603382668:AAHN8FcS90sDaTdN9IHLGu_AKs9R4TcQNs0")
+ADMIN = int(os.environ.get("ADMIN", "2010016480"))
 
 
 # Premium 4GB Renaming Client Config
-STRING_SESSION = os.environ.get("STRING_SESSION", "")
+STRING_SESSION = os.environ.get("STRING_SESSION", "BQGFzVUAxsUqrhGS1aBnjwQbO0ucEFP75lvhObyrZegzX8jmZrCInu-pSrTu97qEuqej1DD2zd2w7VoNP2-rdU57HPNXfXvQChJztWUFJygOUXnFqhLHIEuOgI9sC91wZxwWwrC1grOyw66y1E9ubWxgbo9kZsSyhpgnihsRCkFBFTCvd3X-yAt6JyMLUWpZVl_daD99g59Orso9duK7EYKvDrK_pY34jAvL5yud7tfYgt0CrPjWDBK6o0zyv3O1xlAhagXAPwQW50wZfVbVzGedhBB5I_oTdPJsh5MuEaNRT5dQS7SuBf0F08AAYQPfZgAAaxTnOtjcIwr8pGWKkUeogRoiHAAAAAB3zmrgAA")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "Ongoing_Anime_Florespick")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002446423158"))
 
 
 # Mongo DB Database Config
@@ -24,7 +24,7 @@ DATABASE_NAME = os.environ.get("DATABASE_NAME", "madflixbotz")
 
 
 # Other Variables Config
-START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
+START_PIC = os.environ.get("START_PIC", "https://vault.pictures/p/dfab872437d34c14851a003271dfab59")
 
 
 
